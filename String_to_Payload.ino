5fdecbafc28cc346c8ca363ff462730e73d9ed71
Oltan <ol_tan@hotmail.com>
2022-05-04 21:19:52 +0300

    string to char to int

--- String_to_Payload.ino
+++ String_to_Payload.ino
@@ -1,10 +1,12 @@
 void String_to_Payload(){
   int i=1;
-  while(telemetri_string.length()>i){
-    payload[i-1]=telemetri_string[i-1];
+  int str_len = telemetri_string.length();
+  char char_array[str_len];
+  telemetri_string.toCharArray(char_array,str_len); 
+  while(str_len>i){
+    payload[i-1]=char_array[i-1];
     i++;
   }
-
 
   
 }
