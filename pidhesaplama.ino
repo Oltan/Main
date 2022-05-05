4de43e25d5a389474d27eae8b15a8c38d503210c
Oltan <ol_tan@hotmail.com>
2022-05-05 11:51:11 +0300

    Update pidhesaplama.ino

--- pidhesaplama.ino
+++ pidhesaplama.ino
@@ -1,6 +1,13 @@
 void pidhesaplama(){
   //Roll hesaplamalari
-  
+  double roll_mem = 0;
+  double roll_ki = 0 ;
+  double roll_kp = 0;
+  double roll_kd = 0;
+  double roll_error = 0;
+  double roll_output = 0,
+  roll_error = -roll;
+  roll_output = roll_kp * roll_error + roll_error*roll_ik + roll_kd * ( roll_error - roll_mem ); 
 
     
 
