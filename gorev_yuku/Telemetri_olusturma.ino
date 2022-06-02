afeb045ea98cdd3e6601ad8bab837f8292d7fe24
Oltan <ol_tan@hotmail.com>
2022-06-02 19:52:07 +0300

    hadi deneyin şunu

--- gorev_yuku/Telemetri_olusturma.ino
+++ gorev_yuku/Telemetri_olusturma.ino
@@ -2,6 +2,7 @@ void Telemetri_olusturma(String girdi){
     string irtifa_string[4];
     int irtifa_2;
     double z_hizi=0;
+    double gerilim=11.6;
     telemetri_string += "<";
     telemetri_string += takim_no;
     telemetri_string += ">";
@@ -18,7 +19,7 @@ void Telemetri_olusturma(String girdi){
         telemetri_string += girdi[i];//basinc2yi stringe yazar
     }
     telemetri_string += ",";
-    telemetri_string += gps_yukseklik_string;
+    telemetri_string += "<" + String(yukseklik) + ">,";
     for (int i = 8; i <= 12; i++)
     {
         telemetri_string += girdi[i];//yükseklik2 yazdırılır
@@ -30,10 +31,14 @@ void Telemetri_olusturma(String girdi){
     irtifa_2 = irtifa_string.toint();
     telemetri_string += ",<" + String(alt-irtifa_2) + ">"; 
     telemetri_string += ",<" + String(z_hizi, 2) + ">";
+    telemetri_string += ",<" + String(sicaklik) + ">";
+    telemetri_string += ",<" + String(gerilim,1)+ ">";
     telemetri_string += gps_konum_string + gps_yukseklik_string;
-    telemetri_string += "<uydu_status>,";
-    telemetri_string += "<" + String(pitch) + ">,<" + String(roll) + ">,<" + String(yaw) + ">,<" + "donussayisi" + ">,<" + "video=evet" + ">";
-    telemetri_string += girdi;
+    for (int i = 13; i <= 35; i++)
+    {
+        telemetri_string += girdi[i];
+    }
+    telemetri_string += "<1>,";
+    telemetri_string += "<" + String(pitch) + ">,<" + String(roll) + ">,<" + String(yaw) + ">,<" + "0" + ">,<" + "hayir" + ">";
     Serial.println(girdi);
-    telemetri_string += "\n";
 }
